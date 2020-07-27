fedf2968c9268be5b7734a25bbeb01fb4151d05e
Enrique Mosquea Romero <riki30003@hotmail.com>
2020-07-27 15:07:31 -0400

    The first change

---
+++ App_forthe_Maquina/App_forthe_Maquina/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App_forthe_Maquina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
